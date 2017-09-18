08c16f82a6dc6281b40c2a51463a588724238071
blackpearl80 <31975353+blackpearl80@users.noreply.github.com>
2017-09-18 03:51:13 +0200

    Update LoliNya.py

--- LoliNya.py
+++ LoliNya.py
@@ -11,11 +11,6 @@ description = "LoliNya Bot"
 bot_prefix = "!"
 bot_name = "LoliNya"
 
-# Show a random image when using the command !hug
-hugRnd = random.choice(["a5s3dI6Wv1Qcw","aVmEsdMmCTqSs","od5H3PmEG5EVq","9bbIQ1ZUgHbqw","xZshtXrSgsRHy","IRUb7GTCaPU8E","3ZnBrkqoaI2hq","aD1fI3UUWC4","DjczAlIcyK1Co","3bqtLDeiDtwhq","GMFUrC8E8aWoo"])
-#channel = ctx.message.channel
-response = requests.get("https://media.giphy.com/media/" + hugRnd + "/giphy.gif", stream=True)
-
 client = commands.Bot(description=description, command_prefix=bot_prefix)
 
 @client.event
@@ -60,6 +55,10 @@ async def yorha(ctx):
 
 @client.command(pass_context=True)
 async def hug(ctx, user: discord.Member = None):
+    # Show a random image when using the command !hug
+    hugRnd = random.choice(["a5s3dI6Wv1Qcw","aVmEsdMmCTqSs","od5H3PmEG5EVq","9bbIQ1ZUgHbqw","xZshtXrSgsRHy","IRUb7GTCaPU8E","3ZnBrkqoaI2hq","aD1fI3UUWC4","DjczAlIcyK1Co","3bqtLDeiDtwhq","GMFUrC8E8aWoo"])
+    channel = ctx.message.channel
+    response = requests.get("https://media.giphy.com/media/" + hugRnd + "/giphy.gif", stream=True)
     if not user:
         hugR = random.choice(["*Evades and throw you a pillow.* H-hey nya, don't try hug me! (/ω＼)","Σ(￣ロ￣lll) da heck you are hugging? Are you crazy?","Nya bruh. If you are so desperate to hug something, t-try a train at 100km/h!","Error 404. Object to hug not found. please try again nya!"])
         await client.say(hugR)
@@ -69,7 +68,7 @@ async def hug(ctx, user: discord.Member = None):
         if user.mention == ctx.message.author.mention:
             hugR = random.choice(["So sad. You must be feeling so lonely... (⋟﹏⋞)","Are you feeling cold?","Nya... so pitiful. (´・ω・｀)","Amaaazing nya! You can actually hug yourself! Congratz {}. (￣ε￣〃)ｂ".format(user.mention),"You hugged yourself! That's a nice improvement!"])
             #await client.say(hugR)
-            await client.send_file(ctx.message.channel, io.BytesIO(response.raw.read()), filename=hugRnd + ".gif", content=hugR)
+            await client.send_file(channel, io.BytesIO(response.raw.read()), filename=hugRnd + ".gif", content=hugR)
         elif user.name == client.user.name:
             hugR = random.choice(["T-thankies, I-I guess...","Huggu {}. (.づ◡﹏◡)づ.".format(ctx.message.author.mention),"H-hey, what are you touching! You p-perv! (#｀皿´)","Cuddles {}".format(ctx.message.author.mention), "Pedo much {}? (￣︶￣;)".format(ctx.message.author.mention)])
             await client.say(hugR)
